ae9eb467f61eaccdcac5e1da0fdae54a38999bec
Harry Kunz <harry.kunz@ymail.com>
2016-06-30 06:31:24 -0500

    fix stock chart

--- www/portfolio/stockchart/index.php
+++ www/portfolio/stockchart/index.php
@@ -1,10 +1,23 @@
 <?php
   ob_start();
+  $link = "<a class='basicLink' href='https://github.com/hkunz/pacman-prototype' target='_blank'>github</a>";
+  $swflink = "<a class='basicLink' href='http://www.hkunz.com/portfolio/stockchart/StockTrade.php' target='_blank'>here</a>";
 ?>
 
 <div>
-	<p>Stock Page Specific Content</p>
+	<p>Random stock chart. Code at <?php echo $link;?>. Click <?php echo $swflink;?> for larger view<br/><br/></p>
+	<div><div id='flashContent' name='flashContent' class='flashContent'></div></div>
 </div>
+
+<script language="javascript" type="text/javascript">
+        var root = '../../';
+        var type = 'actionscript';
+        var so = new SWFObject(root + "flash/FlashLoader.swf?url=StockTrade.swf", "theMovie", "445", "590", "8");
+        
+        so.addParam("allowScriptAccess", "always");
+        so.addParam("wmode", "transparent", "middle");
+        so.write("flashContent");
+</script>
 
 <?php
 	$pageTitle = "Stock Chart";
