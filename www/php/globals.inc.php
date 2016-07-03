c7edc23aac87f51787e42b9662e08f116ed0680e
Harry Kunz <harry.kunz@ymail.com>
2016-07-03 20:51:35 +0200

    remove nonsensical localhost check

--- www/php/globals.inc.php
+++ www/php/globals.inc.php
@@ -1,12 +1,4 @@
 <?php
-	//define('ABSPATH', dirname(__FILE__).'/');
-	$server = $_SERVER['SERVER_NAME']; //either "localhost" or "www.hkunz.com"
-	$root = "http://" . $server;
-	if($server == "localhost"){
-		$root = $root . "/projects/hkunz";
-		//include($server . '/projects/lib/php/FirePHPCore/fb.php');
-		//fb("server:");
-	}
-	$root .= "/";
-	//$masterPage = $root . "php/master.inc.php";
+	$server = $_SERVER['SERVER_NAME'];
+	$root = "http://" . $server . "/";
 ?>
