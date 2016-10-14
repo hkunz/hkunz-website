2232c3b217677fea133f8f35571ee4f09a95a1ae
Harry Kunz <harry.kunz@ymail.com>
2016-10-15 00:54:19 +0200

    link itunes

--- www/content/portfolio/content-aftereffects.php
+++ www/content/portfolio/content-aftereffects.php
@@ -6,7 +6,7 @@
 <p class="block">
 This is a music video project i created with <a href="https://en.wikipedia.org/wiki/Adobe_After_Effects" target="_blank">Adobe After Effects</a>!
 I bought a license for the instrumental beat and wrote a lyrics for it which i rapped as <a href="https://www.youtube.com/c/HarryMcKenzieTV" target="_blank">Harry McKenzie</a>. My friend <a href="http://m-speck.weebly.com/" target="_blank">Marika Speck</a> did an awesome job singing the chorus of this single!
-If you are interested, you can buy the song on all major distributions like <a href="https://goo.gl/a0gYBw" target="_blank">Google Play Store</a>, <a href="http://amzn.to/2epw6pA" target="_blank">Amazon</a>, <a href="http://www.cdbaby.com/cd/harrymckenzie" target="_blank">CDBaby</a>, and <a href="http://www.apple.com/itunes/" target="_blank">iTunes</a>
+If you are interested, you can buy the song on all major distributions like <a href="https://itunes.apple.com/de/album/broken-world-single/id1165305193" target="_blank">iTunes</a>, <a href="https://goo.gl/a0gYBw" target="_blank">Google Play</a>, <a href="http://amzn.to/2epw6pA" target="_blank">Amazon</a>, and, <a href="http://www.cdbaby.com/cd/harrymckenzie" target="_blank">CDBaby</a>
 </p>
 <?php createYoutubeVideo("RAhVzVI-xlg"); ?>
 <p>
